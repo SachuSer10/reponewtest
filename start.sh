9f3f2c93e719a83fdb211be2e62df487a6dd2daa
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-10-01 17:51:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mfnewrepo /mfnewrepo
+  git clone https://github.com/SachuSer10/mfnewrepo /mfnewrepo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /mfnewrepo
