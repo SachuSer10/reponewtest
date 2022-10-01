8386957e2187cbff9e67e216373293a0a3e90d4c
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-10-01 17:52:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/SachuSer10/mfnewrepo /mfnewrepo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /mfnewrepo
+  git clone $UPSTREAM_REPO /mfnewrepo 
 fi
-cd /mfnewrepo
+cd /mfnewrepo 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
