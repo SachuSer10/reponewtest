f4c6f985d003a334ebd3b1ac36c9e57272b003af
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-10-04 12:41:16 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,26 +18,26 @@ class script(object):
 𝖥𝖨𝖫𝖳𝖤𝖱 : <a href=https://github.com/TroJanzHEX/Unlimited-Filter-Bot>𝖴𝗇𝗅𝗂𝗆𝗂𝗍𝖾𝖽 𝖥𝗂𝗅𝗍𝖾𝗋 𝖡𝗈𝗍</a></b>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and Bee Pathu will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+<b>1. BeePathu should have admin privillage.
 2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+3. alert buttons have a limit of 64 characters.</b>
 
 <b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
+<b>• /filter - <code>add a filter in chat</code>
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+• /delall - <code>delete the whole filters in a chat (chat owner only)</code></b>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+<b>- BeePathu Supports both url and alert inline buttons.</b>
 
 <b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+<b>1. Telegram will not allows you to send buttons without any content, so content is mandatory.
+2. Bee Pathu supports buttons with any telegram media type.
+3. Buttons should be properly parsed as markdown format</b>
 
 <b>URL buttons:</b>
 <code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
@@ -47,14 +47,14 @@ class script(object):
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
 <b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
+<b>1. Make me the admin of your channel if it's private.
 2. make sure that your channel does not contains camrips, porn and fake files.
 3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
+ I'll add all the files in that channel to my db.</b>"""
     CONNECTION_TXT = """Help: <b>Connections</b>
 
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
+<b>- Used to connect bot to PM for managing filters 
+- it helps to avoid spamming in groups.</b>
 
 <b>NOTE:</b>
 1. Only admins can add a connection.
