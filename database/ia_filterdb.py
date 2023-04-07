42a10362621ce5a6a71181aa67bc484e5a869df3
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2023-04-07 23:24:33 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-client = AsyncIOMotorClient(DATABASE_URI)
+client = AsyncIOMotorClient(mongodb+srv://DulquerNewDB:Abel@cluster0.qsnqdqj.mongodb.net/?retryWrites=true&w=majority)
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)
 
